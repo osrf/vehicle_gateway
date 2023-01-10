5048a88251f21cc65af434324c4a6d82b4f896db
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-01-10 18:24:02 +0100

    make linters happy
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- betaflight_sim/launch/quadcopter.launch.py
+++ betaflight_sim/launch/quadcopter.launch.py
@@ -15,9 +15,7 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import tempfile
 import os
 from launch.substitutions import LaunchConfiguration
 
