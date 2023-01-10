77ef207dee8e041c5e497d843f3e29ac20d75abf
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-01-10 18:09:53 +0100

    Improve betaflight cmakelists
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

---
+++ betaflight_sim/launch/quadcopter.launch.py
@@ -0,0 +1,54 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import tempfile
+import os
+from launch.substitutions import LaunchConfiguration
+
+
+def get_betaflight_dir():
+    return get_package_share_directory('betaflight_sim')
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    run_betaflight = ExecuteProcess(
+        cmd=['betaflight_SITL.elf'],
+        output='screen'
+    )
+
+    # Launch Arguments
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value=use_sim_time,
+        description='If true, use simulated clock')
+
+    world_sdf = os.path.join(get_betaflight_dir(), "worlds", "empty_betaflight_world.sdf")
+
+    return LaunchDescription([
+        # Launch gazebo environment
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('ros_gz_sim'),
+                              'launch', 'gz_sim.launch.py')]),
+            launch_arguments=[('gz_args', [' -r -v 4 ' + world_sdf])]),
+        run_betaflight,
+        use_sim_time_arg,
+        ExecuteProcess(cmd=['betaflight-configurator']),
+    ])
