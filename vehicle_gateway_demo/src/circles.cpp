75048a13a261c55dc88c29a27c06e5bd15a65231
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-03-01 16:25:04 +0100

    circle demo and some other fixes
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- vehicle_gateway_demo/src/circles.cpp
+++ vehicle_gateway_demo/src/circles.cpp
@@ -17,42 +17,25 @@
 #include <thread>
 
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/float64.hpp>
-#include <control_toolbox/pid.hpp>
 
 #include <pluginlib/class_loader.hpp>
 #include <vehicle_gateway/vehicle_gateway.hpp>
 #include <vehicle_gateway_px4/vehicle_gateway_px4.hpp>
 
-#include <aruco_opencv_msgs/msg/aruco_detection.hpp>
-
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
-template<typename T>
-inline T clamp(T _v, T _min, T _max)
-{
-  return std::max(std::min(_v, _max), _min);
-}
-
-class MinimalSubscriber : public rclcpp::Node
+class DroneCircles : public rclcpp::Node
 {
 public:
-  MinimalSubscriber(int argc, const char ** argv)
+  DroneCircles(int argc, const char ** argv)
   : Node("minimal_subscriber"), loader_("vehicle_gateway", "vehicle_gateway::VehicleGateway")
   {
-    this->subscription_marker_ = this->create_subscription<aruco_opencv_msgs::msg::ArucoDetection>(
-      "/aruco_detections", 10,
-      std::bind(&MinimalSubscriber::topic_callback, this, _1));
-
     this->gateway_ = this->loader_.createSharedInstance(
       "vehicle_gateway_px4::VehicleGatewayPX4");
     RCLCPP_INFO(this->get_logger(), "Initializing VehicleGatewayPX4");
     this->gateway_->init(0, nullptr);
-    this->thread_loop_ = std::thread(&MinimalSubscriber::loop, this);
-
-    this->x_control_pid_ = control_toolbox::Pid(0.08, 0, 0);
-    this->y_control_pid_ = control_toolbox::Pid(0.08, 0, 0);
+    this->thread_loop_ = std::thread(&DroneCircles::loop, this);
   }
 
   void loop()
@@ -100,19 +83,6 @@ public:
 
     while(!this->stopped_)
     {
-    //
-    //
-    //   std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
-    //
-    //   auto diff =
-    //     std::chrono::duration_cast<std::chrono::nanoseconds>(now - last_update_time);
-    //   last_update_time = now;
-    //
-      float x, y, z;
-      this->gateway_->get_local_position(x, y, z);
-      RCLCPP_INFO(this->get_logger(), "x %.2f \t y: %.2f \t %.2f", x, y, z);
-      RCLCPP_INFO(this->get_logger(), "->x %.2f \t y: %.2f \t %.2f", this->radius * cos(this->theta), this->radius * sin(this->theta), z);
-
       this->gateway_->set_offboard_control_mode(true);
       this->gateway_->set_local_position_setpoint(
         this->radius * cos(this->theta),
@@ -125,47 +95,13 @@ public:
         std::chrono::duration_cast<std::chrono::milliseconds>(now - last_update_time);
       this->theta = this->theta + this->omega * diff.count() / 1000.0;
 
-      std::cerr << "diff.count() / 1000.0 " << diff.count() / 1000.0 << '\n';
-
       last_update_time = now;
-
-      // std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
-      // {
-      //   std::lock_guard<std::mutex> lock(mutex_marker);
-      //   if (this->marker_detected_)
-      //   {
-      //     RCLCPP_INFO(this->get_logger(), "x %.2f \t y: %.2f",  this->marker_x_,  this->marker_y_);
-      //
-      //     x -= this->marker_x_;
-      //     y -= this->marker_y_;
-      //
-      //     this->marker_x_ = 0;
-      //     this->marker_y_ = 0;
-      //     this->marker_detected_ = false;
-      //     last_update_time = now;
-      //   }
-      // }
-      //
-      // auto diff =
-      //   std::chrono::duration_cast<std::chrono::milliseconds>(now - last_update_time);
-      //
-      // if (diff.count() > 1000)
-      // {
-      //   this->gateway_->set_local_position_setpoint(0, 0, -5);
-      //   RCLCPP_INFO(this->get_logger(), "Marker is not visible");
-      //
-      // }
-      // else
-      // {
-      //   this->gateway_->set_local_position_setpoint(x, y, -5);
-      // }
       std::this_thread::sleep_for(50ms);
     }
     RCLCPP_INFO(this->get_logger(), "Vehicle end");
-
   }
 
-  ~MinimalSubscriber()
+  ~DroneCircles()
   {
     this->stopped_ = true;
     this->thread_loop_.join();
@@ -173,35 +109,8 @@ public:
   }
 
 private:
-  void topic_callback(const aruco_opencv_msgs::msg::ArucoDetection & msg)
-  {
-    std::lock_guard<std::mutex> lock(mutex_marker);
-
-    if (msg.markers.size() > 0)
-    {
-      auto marker = msg.markers[0];
-      this->marker_x_ = marker.pose.position.x;
-      this->marker_y_ = marker.pose.position.y;
-      this->marker_z_ = marker.pose.position.z;
-      this->marker_detected_ = true;
-    }
-  }
-
-  std::mutex mutex_marker;
-  float marker_x_;
-  float marker_y_;
-  float marker_z_;
-  bool marker_detected_{false};
-
-  float x_{0};
-  float y_{0};
-  float z_{0};
-  control_toolbox::Pid x_control_pid_;
-  control_toolbox::Pid y_control_pid_;
-
   pluginlib::ClassLoader<vehicle_gateway::VehicleGateway> loader_;
   std::shared_ptr<vehicle_gateway::VehicleGateway> gateway_;
-  rclcpp::Subscription<aruco_opencv_msgs::msg::ArucoDetection>::SharedPtr subscription_marker_;
   std::thread thread_loop_;
   bool stopped_ = false;
 
@@ -213,7 +122,7 @@ private:
 int main(int argc, const char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>(argc, argv));
+  rclcpp::spin(std::make_shared<DroneCircles>(argc, argv));
   rclcpp::shutdown();
   return 0;
 }
