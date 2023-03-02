1e0b2baa0fa7fbc158ce0dec14bdfc6f7c1197ad
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-03-02 09:58:40 +0100

    Added velocity control to PX4 API (#36)
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- vehicle_gateway_python/examples/velocity_control.py
+++ vehicle_gateway_python/examples/velocity_control.py
@@ -44,8 +44,8 @@ while 1:
                 print('Arming')
                 time.sleep(0.01)
 
-        px4_gateway.set_offboard_control_mode(ControllerType.POSITION)
-        px4_gateway.set_local_position_setpoint(0, -5, -5, 0)
+        px4_gateway.set_offboard_control_mode(ControllerType.VELOCITY)
+        px4_gateway.set_local_velocity_setpoint(0, 0, -5, 5)
 
         _start_time = current_time
     time.sleep(0.1)
