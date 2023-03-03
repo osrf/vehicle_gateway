d61bdd50f0ecaee192703aeb7507f1064ea062a8
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-03-03 12:15:58 +0100

    make linters happy
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- vehicle_gateway_integration_test/test/script/arm_disarm.py
+++ vehicle_gateway_integration_test/test/script/arm_disarm.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import time
 import sys
+import time
 
 import vehicle_gateway
 from vehicle_gateway import ArmingState
