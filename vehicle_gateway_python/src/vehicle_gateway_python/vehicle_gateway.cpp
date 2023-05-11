8350eb5e83ed4623d3526e90c171f6355570f269
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-05-11 17:48:39 +0200

    Avoid blocking command line when sigterm is emitted (#109)
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- vehicle_gateway_python/src/vehicle_gateway_python/vehicle_gateway.cpp
+++ vehicle_gateway_python/src/vehicle_gateway_python/vehicle_gateway.cpp
@@ -77,7 +77,9 @@ void VehicleGatewayPython::Arm()
 
 void VehicleGatewayPython::ArmSync()
 {
-  while (this->gateway_->get_arming_state() != vehicle_gateway::ARMING_STATE::ARMED) {
+  while (this->gateway_->get_arming_state() != vehicle_gateway::ARMING_STATE::ARMED &&
+    rclcpp::ok())
+  {
     this->gateway_->arm();
     usleep(1e5);  // 100 ms
   }
@@ -90,7 +92,9 @@ void VehicleGatewayPython::Disarm()
 
 void VehicleGatewayPython::DisarmSync()
 {
-  while (this->gateway_->get_arming_state() != vehicle_gateway::ARMING_STATE::STANDBY) {
+  while (this->gateway_->get_arming_state() != vehicle_gateway::ARMING_STATE::STANDBY &&
+    rclcpp::ok())
+  {
     this->gateway_->disarm();
     usleep(1e5);  // 100 ms
   }
@@ -103,7 +107,9 @@ void VehicleGatewayPython::TransitionToMultiCopter()
 
 void VehicleGatewayPython::TransitionToMultiCopterSync()
 {
-  while (this->gateway_->get_vtol_state() != vehicle_gateway::VTOL_STATE::MC) {
+  while (this->gateway_->get_vtol_state() != vehicle_gateway::VTOL_STATE::MC &&
+    rclcpp::ok())
+  {
     this->gateway_->transition_to_mc();
     usleep(1e5);  // 100 ms
   }
@@ -116,7 +122,9 @@ void VehicleGatewayPython::TransitionToFixedWings()
 
 void VehicleGatewayPython::TransitionToFixedWingsSync()
 {
-  while (this->gateway_->get_vtol_state() != vehicle_gateway::VTOL_STATE::FW) {
+  while (this->gateway_->get_vtol_state() != vehicle_gateway::VTOL_STATE::FW &&
+    rclcpp::ok())
+  {
     this->gateway_->transition_to_fw();
     usleep(1e5);  // 100 ms
   }
