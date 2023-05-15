6ef790def34cc6c493bb1e11ebb01d7f709b0c69
Morgan Quigley <morgan@osrfoundation.org>
2023-05-15 11:52:45 -0400

    send telemetry as JSON message to start
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- vehicle_gateway_multi/src/vehicle_gateway_multi_bridge.cpp
+++ vehicle_gateway_multi/src/vehicle_gateway_multi_bridge.cpp
@@ -12,13 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <unistd.h>
+
 #include <cstdio>
+#include <string>
+
+#include "nlohmann/json.hpp"
 #include "zenoh.h"
+
+using std::string;
+using nlohmann::json;
 
 int main(int argc, char ** argv)
 {
   if (argc < 2) {
-    printf("usage: vehicle_gateway_multi_bridge CONFIG_FILENAME\n");
+    printf("usage: vehicle_gateway_multi_bridge ZENOH_CONFIG_FILENAME VEHICLE_ID\n");
     return EXIT_FAILURE;
   }
   const char * config_filename = argv[1];
@@ -34,6 +42,27 @@ int main(int argc, char ** argv)
     return EXIT_FAILURE;
   }
   printf("zenoh session open!\n");
+
+  // todo: construct subscribers to vehicle position ROS 2 topic
+
+  json j;
+
+  // todo: exit loop as signaled by ROS 2
+  while (true) {
+    printf("sending telemetry message...\n");
+    // time of telemetry data
+    j["sec"] = 42;
+    j["nsec"] = 43;
+    // telemetry: NED position, add other stuff in future?
+    j["east"] = 1.234;
+    j["north"] = 2.345;
+    j["down"] = -3.456;
+
+    // todo: send string message via zenoh
+    string s = j.dump();
+
+    usleep(1000000);  // todo: use ROS 2 timer for pacing
+  }
 
   printf("closing zenoh session...\n");
   z_close(z_move(session));
