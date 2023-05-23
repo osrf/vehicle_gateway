a6dd590d3f5c65863a8ea5383fed9e947de58212
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-05-23 13:59:44 +0200

    more cleanups
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- vehicle_gateway_demo/launch/px4_demo.launch.py
+++ vehicle_gateway_demo/launch/px4_demo.launch.py
@@ -13,114 +13,18 @@
 # limitations under the License.
 
 from ament_index_python.packages import get_package_share_directory
-from distutils.dir_util import copy_tree
-from launch import LaunchDescription, Substitution, SomeSubstitutionsType, LaunchContext
+from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.actions import RegisterEventHandler, SetEnvironmentVariable
 from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from vehicle_gateway_python_helpers.helpers import get_model_pose, get_px4_dir, seed_rootfs
 
 import tempfile
 import os
 from launch.substitutions import LaunchConfiguration
-from typing import List
-import xml.etree.ElementTree as ET
-
-
-class WorldPoseFromSdfFrame(Substitution):
-    """Substitution that retrieves a frame from SDF."""
-
-    def __init__(self,
-                 frame_name: SomeSubstitutionsType,
-                 world_name: SomeSubstitutionsType,
-                 model_pose: SomeSubstitutionsType,
-                 coor_name: str) -> None:
-        super().__init__()
-
-        from launch.utilities import normalize_to_list_of_substitutions  # import here to avoid loop
-        self.__frame_name = normalize_to_list_of_substitutions(frame_name)
-        self.__world_name = normalize_to_list_of_substitutions(world_name)
-        self.__model_pose = normalize_to_list_of_substitutions(model_pose)
-        self.__x = "0.0"
-        self.__y = "0.0"
-        self.__z = "0.3"
-        self.__roll = "0.0"
-        self.__pitch = "0.0"
-        self.__yaw = "0.0"
-        self.__coord_name = coor_name
-
-    @property
-    def model_pose(self) -> List[Substitution]:
-        """Getter for model pose."""
-        return self.__model_pose
-
-    @property
-    def frame_name(self) -> List[Substitution]:
-        """Getter for frame name."""
-        return self.__frame_name
-
-    @property
-    def world_name(self) -> List[Substitution]:
-        """Getter for world name."""
-        return self.__world_name
-
-    def parseCoords(self, strCoords: str, key: str, strSplit: str):
-        x, y, z, roll, pitch, yaw = strCoords.split(strSplit)
-        if (self.__coord_name == "x"):
-            return str(x)
-        if (self.__coord_name == "y"):
-            return str(y)
-        if (self.__coord_name == "z"):
-            return str(z)
-        if (self.__coord_name == "roll"):
-            return str(roll)
-        if (self.__coord_name == "pitch"):
-            return str(pitch)
-        if (self.__coord_name == "yaw"):
-            return str(yaw)
-        return "0.0"
-
-    def perform(self, context: LaunchContext) -> str:
-        from launch.utilities import perform_substitutions
-        frame_name_str = perform_substitutions(context, self.frame_name)
-        world_name_str = perform_substitutions(context, self.world_name)
-        model_pose_str = perform_substitutions(context, self.model_pose)
-
-        # allow manually specified model_pose param to override lookup
-        if model_pose_str != '':
-            return self.parseCoords(model_pose_str, self.__coord_name, ", ")
-
-        if frame_name_str != '':
-            world_sdf_path = os.path.join(
-                get_package_share_directory('vehicle_gateway_worlds'),
-                'worlds',
-                world_name_str + '.sdf')
-            # I couldn't get the libsdformat binding to work as expected due
-            # to various troubles. Let's simplify and just treat SDF as
-            # regular XML and do an XPath query
-            sdf_root = ET.parse(world_sdf_path).getroot()
-            frame_node = sdf_root.find(f'.//frame[@name=\'{frame_name_str}\']')
-            if not frame_node:
-                raise ValueError(f'Could not find a frame named {frame_name_str}')
-            pose_node = frame_node.find('pose')
-            pose_str = pose_node.text
-            # SDFormat stores poses space-separated, but we need them comma-separated
-            return self.parseCoords(pose_str, self.__coord_name, " ")
-
-        # default a bit above the origin; vehicle will drop to the ground plane
-        return self.parseCoords("0, 0, 0.3, 0, 0, 0", self.__coord_name, ", ")
-
-
-def get_px4_dir():
-    return get_package_share_directory('px4_sim')
-
-
-def seed_rootfs(rootfs):
-    px4_dir = get_px4_dir()
-    print(f'seeding rootfs at {rootfs} from {px4_dir}')
-    copy_tree(px4_dir, rootfs)
 
 
 def generate_launch_description():
@@ -153,41 +57,11 @@ def generate_launch_description():
                                            default_value=model_pose,
                                            description='Model pose (x, y, z, roll, pitch, yaw)')
 
-    model_pose_x = WorldPoseFromSdfFrame(
-        frame_name=LaunchConfiguration('frame_name'),
-        world_name=LaunchConfiguration('world_name'),
-        model_pose=LaunchConfiguration('model_pose'),
-        coor_name='x')
-
-    model_pose_y = WorldPoseFromSdfFrame(
-        frame_name=LaunchConfiguration('frame_name'),
-        world_name=LaunchConfiguration('world_name'),
-        model_pose=LaunchConfiguration('model_pose'),
-        coor_name='y')
-
-    model_pose_z = WorldPoseFromSdfFrame(
-        frame_name=LaunchConfiguration('frame_name'),
-        world_name=LaunchConfiguration('world_name'),
-        model_pose=LaunchConfiguration('model_pose'),
-        coor_name='z')
-
-    model_pose_roll = WorldPoseFromSdfFrame(
-        frame_name=LaunchConfiguration('frame_name'),
-        world_name=LaunchConfiguration('world_name'),
-        model_pose=LaunchConfiguration('model_pose'),
-        coor_name='roll')
-
-    model_pose_pitch = WorldPoseFromSdfFrame(
-        frame_name=LaunchConfiguration('frame_name'),
-        world_name=LaunchConfiguration('world_name'),
-        model_pose=LaunchConfiguration('model_pose'),
-        coor_name='pitch')
-
-    model_pose_yaw = WorldPoseFromSdfFrame(
-        frame_name=LaunchConfiguration('frame_name'),
-        world_name=LaunchConfiguration('world_name'),
-        model_pose=LaunchConfiguration('model_pose'),
-        coor_name='yaw')
+    [model_pose_x, model_pose_y, model_pose_z,
+     model_pose_roll, model_pose_pitch, model_pose_yaw] = get_model_pose(
+        LaunchConfiguration('frame_name'),
+        LaunchConfiguration('world_name'),
+        LaunchConfiguration('model_pose'))
 
     world_pkgs = get_package_share_directory('vehicle_gateway_worlds')
 
